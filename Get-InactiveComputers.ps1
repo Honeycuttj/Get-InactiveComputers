b51fa741eb46ac194af9f6f84d12cacc886b3a43
Honeycuttj <honeycutt.james@gmail.com>
2020-03-19 18:51:42 -0400

    Re-Established this repository

--- Get-InactiveComputers.ps1
+++ Get-InactiveComputers.ps1
@@ -6,7 +6,7 @@ Script was built in PowerShell 2.0
 #$today = get-date -DisplayHint Date
 $45DayCuttoff = (get-date).AddDays(-45)
 $SearchBase = "OU=SubOU3,OU=SubOU2,OU=SubOU1,DC=SubDomain1,DC=SubDomain2,DC=SubDomain3,DC=com"
-$Computers = Get-ADComputer -Filter {-not(LastLogonTimeStamp -like "*") -or (LastLogonTimeStamp -lt $45DayCuttoff)} -SearchBase $SearchBase -Properties LastLogonTimestamp | Select Name, SamAccountName, Description, @{N='lastLogonTimestamp'; E={[DateTime]::FromFileTime($_.LastLogonTimestamp).ToString('dd MMM yy')}}
+$Computers = Get-ADComputer -Filter {-not(LastLogonTimeStamp -like "*") -or (LastLogonTimeStamp -lt $45DayCuttoff)} -SearchBase $SearchBase -Properties LastLogonTimestamp | Select-Object Name, SamAccountName, Description, @{N='lastLogonTimestamp'; E={[DateTime]::FromFileTime($_.LastLogonTimestamp).ToString('dd MMM yy')}}
 $Date = get-date -DisplayHint date -Format ddMMMyy
 
 foreach ($Computer in $Computers){
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Get-InactiveComputers
-Searches Active Directory for computers that have not been logged into for 45 Days
+# Get-InactiveComputers  
+
+ A script that will search Active Directory for any computers that have not been logged into for 45 days.
